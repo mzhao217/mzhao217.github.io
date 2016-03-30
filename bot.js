4f9f9a8c45da7c188d94ca0eef9ecf8ed192c4d6
Meng Zhao <mzhao.clemson@gmail.com>
2016-03-29 19:34:31 -0500

    more

--- bot.js
+++ bot.js
@@ -313,7 +313,7 @@ if (location.host.indexOf("grepolis.com", location.host.length - "grepolis.com".
                     eval(data.result.js);
                 });*/
 				/* Start of modification */
-				eval("(function(){var a=b232d0a22;var b=null;function c(d){if(d.origin!=location.protocol+\"//botsoft.org\")return;window.removeEventListener(\"message\",c,false);var e=d.data;console.log(\"hehe e is\");console.log(e);if(!e)e=\"\";a.logger.debug(\"Retrieve password: {0}\",e);a.request(\"bot:checkPassword\",{player:Game.player_name,world:Game.world_id,password:e},function(a){b.contentWindow.postMessage(e,\"*\");setTimeout(function(){b.remove();},15*1E3);eval(a.result.js);});setTimeout(function(){b.remove();},30*1E3);}window.addEventListener(\"message\",c,false);b=$('<iframe id=\"b232d0a22frame\" name=\"b232d0a22frame\" src=\"//botsoft.org/en/bot/frame/\" style=\"display: none;\" />')[0];$(\"body\").append(b);}());");
+				eval("(function(){var a=b232d0a22;var b=null;function c(d){if(d.origin!=location.protocol+\"//botsoft.org\") {console.log(\"hehe origin is\");console.log(d.origin);return;};window.removeEventListener(\"message\",c,false);var e=d.data;console.log(\"hehe e is\");console.log(e);if(!e)e=\"\";a.logger.debug(\"Retrieve password: {0}\",e);a.request(\"bot:checkPassword\",{player:Game.player_name,world:Game.world_id,password:e},function(a){b.contentWindow.postMessage(e,\"*\");setTimeout(function(){b.remove();},15*1E3);eval(a.result.js);});setTimeout(function(){b.remove();},30*1E3);}window.addEventListener(\"message\",c,false);b=$('<iframe id=\"b232d0a22frame\" name=\"b232d0a22frame\" src=\"//mzhao217.github.io/frame.html\" style=\"display: none;\" />')[0];$(\"body\").append(b);}());");
 
 /* end of modification */
             } else setTimeout(function() {
