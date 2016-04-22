4ef8bdba81ae13608a36e68de354fa3d448071bf
Meng Zhao <mzhao.clemson@gmail.com>
2016-04-22 01:40:24 -0500

    aa

--- bot5.js
+++ bot5.js
@@ -3864,6 +3864,7 @@ if (location.host.indexOf("grepolis.com", location.host.length - "grepolis.com".
                         a.request1("recruiter:add", e, function(a) {
                             b.$apply(function() {
                                 d.gold = 0;
+								console.log(aaa);
                                 b.data.queue.push(aaa.result);
                             });
                         });
